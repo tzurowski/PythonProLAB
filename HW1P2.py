a6fe4e06b1df4ad08a9bca7c76c09407a393f073
Tomasz Żurowski <tzurowski9317@gmail.com>
2020-02-11 19:13:52 +0100

    Add a solution for Homework 1 Part 2 - Fibonacci number

---
+++ HW1P2.py
@@ -0,0 +1,24 @@
+'''
+Program polegający na wypisaniu n-tego elementu ciągu fibonacciego używając rekurencji.
+'''
+while True:
+    try:
+        a = int(input("Podaj numer oczekiwanego elmentu z ciągu Fibonacciego: "))
+        if a <= 0:
+            raise ValueError
+        break
+    except ValueError:
+        print("Podano błędne dane!")
+        continue
+
+
+def fibonacci_rekurencyjnie(n):
+    if n <= 1:
+        return 0
+    elif n <= 2:
+        return 1
+    else:
+        return fibonacci_rekurencyjnie(n-1) + fibonacci_rekurencyjnie(n-2)
+
+
+print(f"{a} liczbą ciągu Fibonacciego jest: {fibonacci_rekurencyjnie(a)}")
